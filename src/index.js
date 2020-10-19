33651c3f6564074af89258e8ead067f3ea1bacb8
ILLIA <illia@MacBook-Pro-ILLIA.local>
2020-10-19 23:03:27 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(arr) {
+    let newArr = [];
+    if (!Array.isArray(arr)) {
+        return [];
+    } else {
+        for (let i = 0; i < arr.length; i++) {
+            if (i % 2 === 0) {
+                newArr = newArr.concat(arr[i]);
+            } else {
+                newArr = newArr.concat(arr[i].reverse());
+            }
+        }
+        return newArr;
+    }
+};
